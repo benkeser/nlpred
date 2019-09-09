e1da94d86abb72e824ea327ce0e80ded83016037
David Benkeser <benkeser@emory.edu>
2019-09-08 22:26:17 -0400

    update docs

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -198,42 +198,6 @@ NULL
 #' @source \url{https://archive.ics.uci.edu/ml/datasets/Cardiotocography}
 NULL
 
-#' default
-#' 
-#' "Default of credit card clients" data from UCI Machine Learning Repository. Raw data have been
-#' processed and an \code{outcome} column added. 
-#' 
-#' Data Set Information (copied from UCI):
-#' 
-#' This research aimed at the case of customers default payments in Taiwan and compares the predictive accuracy of probability of default among six data mining methods. From the perspective of risk management, the result of predictive accuracy of the estimated probability of default will be more valuable than the binary result of classification - credible or not credible clients. Because the real probability of default is unknown, this study presented the novel "Sorting Smoothing Method" to estimate the real probability of default. With the real probability of default as the response variable (Y), and the predictive probability of default as the independent variable (X), the simple linear regression result (Y = A + BX) shows that the forecasting model produced by artificial neural network has the highest coefficient of determination; its regression intercept (A) is close to zero, and regression coefficient (B) to one. Therefore, among the six data mining techniques, artificial neural network is the only one that can accurately estimate the real probability of default.
-#' 
-#' Attribute Information:
-#' 
-#' This research employed a binary variable, default payment (Yes = 1, No = 0), as the response variable. This study reviewed the literature and used the following 23 variables as explanatory variables: 
-#' 
-#' X1: Amount of the given credit (NT dollar): it includes both the individual consumer credit and his/her family (supplementary) credit. 
-#' 
-#' X2: Gender (1 = male; 2 = female). 
-#' 
-#' X3: Education (1 = graduate school; 2 = university; 3 = high school; 4 = others). 
-#' 
-#' X4: Marital status (1 = married; 2 = single; 3 = others). 
-#' 
-#' X5: Age (year). 
-#' 
-#' X6 - X11: History of past payment. We tracked the past monthly payment records (from April to September, 2005) as follows: X6 = the repayment status in September, 2005; X7 = the repayment status in August, 2005; . . .;X11 = the repayment status in April, 2005. The measurement scale for the repayment status is: -1 = pay duly; 1 = payment delay for one month; 2 = payment delay for two months; . . .; 8 = payment delay for eight months; 9 = payment delay for nine months and above. 
-#' 
-#' X12-X17: Amount of bill statement (NT dollar). X12 = amount of bill statement in September, 2005; X13 = amount of bill statement in August, 2005; . . .; X17 = amount of bill statement in April, 2005. 
-#' 
-#' X18-X23: Amount of previous payment (NT dollar). X18 = amount paid in September, 2005; X19 = amount paid in August, 2005; . . .;X23 = amount paid in April, 2005. 
-#' 
-#' @name default
-#' @docType data
-#' @keywords data
-#' @source \url{https://archive.ics.uci.edu/ml/datasets/default+of+credit+card+clients}
-#' @references Yeh, I. C., & Lien, C. H. (2009). The comparisons of data mining techniques for the predictive accuracy of probability of default of credit card clients. Expert Systems with Applications, 36(2), 2473-2480.
-NULL
-
 #' drugs
 #' 
 #' "Drug consumption (quantified) Data Set" from UCI Machine Learning Repository. 
@@ -264,63 +228,6 @@ NULL
 #' @keywords data
 #' @source \url{https://archive.ics.uci.edu/ml/datasets/Drug+consumption+\%28quantified\%29}
 #' @references \url{https://arxiv.org/abs/1506.06297}
-NULL
-
-
-#' magic
-#' 
-#' "MAGIC Gamma Telescope Data Set" from UCI Machine Learning Repository. Raw data have
-#' been processed and an \code{outcome} column added. 
-#' 
-#' Data Set Information (copied from UCI):
-#' 
-#' The data are MC generated (see below) to simulate registration of high energy gamma particles in a ground-based atmospheric Cherenkov gamma telescope using the imaging technique. Cherenkov gamma telescope observes high energy gamma rays, taking advantage of the radiation emitted by charged particles produced inside the electromagnetic showers initiated by the gammas, and developing in the atmosphere. This Cherenkov radiation (of visible to UV wavelengths) leaks through the atmosphere and gets recorded in the detector, allowing reconstruction of the shower parameters. The available information consists of pulses left by the incoming Cherenkov photons on the photomultiplier tubes, arranged in a plane, the camera. Depending on the energy of the primary gamma, a total of few hundreds to some 10000 Cherenkov photons get collected, in patterns (called the shower image), allowing to discriminate statistically those caused by primary gammas (signal) from the images of hadronic showers initiated by cosmic rays in the upper atmosphere (background). 
-#' Typically, the image of a shower after some pre-processing is an elongated cluster. Its long axis is oriented towards the camera center if the shower axis is parallel to the telescope's optical axis, i.e. if the telescope axis is directed towards a point source. A principal component analysis is performed in the camera plane, which results in a correlation axis and defines an ellipse. If the depositions were distributed as a bivariate Gaussian, this would be an equidensity ellipse. The characteristic parameters of this ellipse (often called Hillas parameters) are among the image parameters that can be used for discrimination. The energy depositions are typically asymmetric along the major axis, and this asymmetry can also be used in discrimination. There are, in addition, further discriminating characteristics, like the extent of the cluster in the image plane, or the total sum of depositions. 
-#' 
-#' The data set was generated by a Monte Carlo program, Corsika, described in: 
-#' 
-#' D. Heck et al., CORSIKA, A Monte Carlo code to simulate extensive air showers, 
-#' Forschungszentrum Karlsruhe FZKA 6019 (1998).
-#' 
-#' The program was run with parameters allowing to observe events with energies down to below 50 GeV.
-#' 
-#' Attribute Information:
-#' 
-#' 1. fLength: continuous # major axis of ellipse [mm] 
-#' 
-#' 2. fWidth: continuous # minor axis of ellipse [mm] 
-#' 
-#' 3. fSize: continuous # 10-log of sum of content of all pixels [in #phot] 
-#' 
-#' 4. fConc: continuous # ratio of sum of two highest pixels over fSize [ratio] 
-#' 
-#' 5. fConc1: continuous # ratio of highest pixel over fSize [ratio] 
-#' 
-#' 6. fAsym: continuous # distance from highest pixel to center, projected onto major axis [mm] 
-#' 
-#' 7. fM3Long: continuous # 3rd root of third moment along major axis [mm] 
-#' 
-#' 8. fM3Trans: continuous # 3rd root of third moment along minor axis [mm] 
-#' 
-#' 9. fAlpha: continuous # angle of major axis with vector to origin [deg] 
-#' 
-#' 10. fDist: continuous # distance from origin to center of ellipse [mm] 
-#' 
-#' 11. class: g,h # gamma (signal), hadron (background) 
-#' 
-#' g = gamma (signal): 12332 
-#' 
-#' h = hadron (background): 6688 
-#' 
-#' For technical reasons, the number of h events is underestimated. In the real data, the h class represents the majority of the events. 
-#' 
-#' The simple classification accuracy is not meaningful for this data, since classifying a background event as signal is worse than classifying a signal event as background. For comparison of different classifiers an ROC curve has to be used. The relevant points on this curve are those, where the probability of accepting a background event as signal is below one of the following thresholds: 0.01, 0.02, 0.05, 0.1, 0.2 depending on the required quality of the sample of the accepted events for different experiments.
-#' 
-#' @source \url{https://archive.ics.uci.edu/ml/datasets/MAGIC+Gamma+Telescope}
-#' @references Bock, R.K., Chilingarian, A., Gaug, M., Hakl, F., Hengstebeck, T., Jirina, M., Klaschka, J., Kotrc, E., Savicky, P., Towers, S., Vaicilius, A., Wittek W. (2004). Methods for multidimensional event classification: a case study using images from a Cherenkov gamma-ray telescope. Nucl.Instr.Meth. A, 516, pp. 511-528. 
-#' @name magic
-#' @keywords data
-#' @docType data
 NULL
 
 #' wine
